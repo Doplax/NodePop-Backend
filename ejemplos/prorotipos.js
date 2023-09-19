74cf5da79167effbca4f230f5d023d2cdf72d0c9
Doplax <doplax@gmail.com>
2023-09-19 19:49:40 +0200

    Forma canonica de usar la herencia en js

--- ejemplos/prorotipos.js
+++ ejemplos/prorotipos.js
@@ -9,9 +9,49 @@ function Persona(nombre) {
 const pepe = new Persona('Pepe');
 const luis = new Persona('Luis');
 
-Persona.prototype.saluda = function() { console.log('saluda', this.nombre)}
+Persona.prototype.saluda = function() { console.log('Hola soy', this.nombre)}
 
 console.log(pepe)
 
 ////////////////////////////////
 
+// Herencia simple
+
+function Agente(nombre) {
+    // Heredar el contructor de las personas
+    // Heredar las propiedades de las personas
+
+    // Ejecutar el constructor de las personas con mi 'this'
+
+    Persona.call(this, nombre);
+}
+
+// Forma 1
+//Agente.prototype = Object.create(Persona.prototype) // ?? para que sirve esto?
+//Agente.prototype.contructor = Agente;
+
+// Forma 2
+Object.setPrototypeOf(Agente.prototype, Persona.prototype) 
+
+const smith  = new Agente('Smith');
+
+smith.saluda();
+
+
+// Herencia múltiple ///////////
+
+// Quiero que los agentes ereden tanto de las personas, como de los superheroes 
+
+function Superheroe(){
+    this.vuela = function(){ console.log(this.nombre, 'vuela')}
+}
+
+
+// Copiar tidas las propiedades de los Superheroes al prototio del Agente
+Object.assign(Agente.prototype, new Superheroe())
+
+smith.vuela();
+
+console.log(smith);
+console.log(Agente.prototype);
+console.log(Persona.prototype);
