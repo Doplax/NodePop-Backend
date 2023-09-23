0d3c021d86a745a8afb2d86db6697e0a42cc66c8
Doplax <doplax@gmail.com>
2023-09-23 17:14:40 +0200

    Guardado

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -4,8 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+//var indexRouter = ;
+//var usersRouter = ;
 
 var app = express();
 
@@ -13,14 +14,20 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use((req, res, next) => {
+//  console.log('Ha llegado una petición a', req.url);
+//  next();
+//})
+
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- nodeapp/routes/index.js
+++ nodeapp/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/facturas', (req, res, next) => {
+  res.send('lista de facturas')
+})
+
 module.exports = router;
