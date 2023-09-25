8711827e11f335439e72875cb16757e2c3e4e104
Doplax <doplax@gmail.com>
2023-09-25 19:45:30 +0200

    Creacion del fichero promise.js

---
+++ ejemplos/promise.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// Funcion que devuelve una promesa
+
+function sleep(ms){
+    return new Promise((resolve, reject) =>{
+        setTimeout(() => {
+            resolve(54)
+        },ms)
+    })
+}
+
+
+const promesa = sleep(3000);
+
+console.log(promesa);
+
+promesa.then((resultado) => {
+    console.log('han pasado 3 segundos, con resultado', resuldato)
+    return sleep(2000)
+}).then(() => {
+    console.log("han pasado otros 2 segundos");
+}).catch(err => {
+    console.log('Hubo un error:', err.message); // message está bien?
+})
