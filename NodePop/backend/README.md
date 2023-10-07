290e97da54f37fec9aa4de1bd58ed40a538ff721
Doplax <doplax@gmail.com>
2023-10-07 10:48:50 +0200

    Creacion de sistema para borrar y crear agentes desde 0

--- NodePop/backend/README.md
+++ NodePop/backend/README.md
@@ -10,12 +10,19 @@ $ npm install
 
 Review database connection on  /lib/connectMongoose.js 
 
+Load initial data: 
+```sh   
+#This command deletes all the data in the database and create default database
+npm run init-db
+```
 
 ## Start
 In production 
 ```sh
 npm start
 ```
+
+
 
 In development;
 ```sh
--- NodePop/backend/init-db.js
+++ NodePop/backend/init-db.js
@@ -1,41 +1,66 @@
 // Para resetear la base de datos de la aplicación
-'use strict';
+"use strict";
 
+const readline = require("node:readline"); // 'node:' Se usa para especificar que es nativa de node
 
-const connection = require('./lib/connectMongoose')
-const Agente = require('./models/Agente')
+const connection = require("./lib/connectMongoose");
+const Agente = require("./models/Agente");
+const { resolve } = require("node:path");
 
-main().catch(err => console.error(err));
+main().catch((err) => console.error(err));
 
 async function main() {
-    // Inicializar la colección de agentes
+    // Espero a que se conecte a la base de datos
+    await new Promise(resolve => connection.once('open', resolve)); // Con esto se evita que aparezca este mensaje pegado al de func pregunta()
 
+    const borrar = await pregunta(
+        "Estas seguro de que quieres borrar la base de datos y cargar datos iniciales?"
+    );
+    if (!borrar) {
+        process.exit();
+    }
+
+    // Inicializar la colección de agentes
     await initAgentes();
 
     await connection.close();
 }
 
-
 async function initAgentes() {
-    // Borrar todos los documentos de la colección de agentes
-    const deleted = await Agente.deleteMany();
-    console.log(`Eliminados ${deleted.deletesCount} agentes.`);
+  // Borrar todos los documentos de la colección de agentes
+  const deleted = await Agente.deleteMany();
+  console.log(`Eliminados ${deleted.deletedCount} agentes.`);
 
-    //Crear agentes iniciales 
-    const inserted = await Agente.insertMany([
-        { name: 'John', age: 28 },
-        { name: 'Jane', age: 45 },
-        { name: 'Emily', age: 22 },
-        { name: 'Michael', age: 35 },
-        { name: 'Sarah', age: 40 },
-        { name: 'David', age: 29 },
-        { name: 'Lucy', age: 31 },
-        { name: 'Daniel', age: 24 },
-        { name: 'Sophia', age: 55 },
-        { name: 'Jack', age: 18 }
-    ])
-    console.log(`Creados ${inserted.length} agentes.`)
+  //Crear agentes iniciales
+  const inserted = await Agente.insertMany([
+    { name: "John", age: 28 },
+    { name: "Jane", age: 45 },
+    { name: "Emily", age: 22 },
+    { name: "Michael", age: 35 },
+    { name: "Sarah", age: 40 },
+    { name: "David", age: 29 },
+    { name: "Lucy", age: 31 },
+    { name: "Daniel", age: 24 },
+    { name: "Sophia", age: 55 },
+    { name: "Jack", age: 18 },
+  ]);
+  console.log(`Creados ${inserted.length} agentes.`);
 }
 
-
-
+function pregunta(texto) {
+  return new Promise(function (resolve, reject) {
+    // Conectart readline con la consola
+    const ifc = readline.createInterface({ // ifc = Interface es un palabra reservada, por eso usamos esta
+      input: process.stdin,
+      output: process.stdout,
+    });
+    ifc.question(texto, (respuesta) => {
+        ifc.close();
+        if (respuesta.toLocaleLowerCase() === 'si') {
+            resolve(true);
+        } else {
+            resolve(false);
+        }
+    });
+  });
+}
