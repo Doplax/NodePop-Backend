7bfbad20134befc83d5c40c281a0b359d7e43493
Doplax <doplax@gmail.com>
2024-02-03 13:12:48 +0100

    refactor(app): Modularization of app logic in corresponding folders

--- src/app.js
+++ src/app.js
@@ -1,97 +1,51 @@
-const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-//const MongoStore = require('connect-mongo');
-
-//const session = require('express-session');
 const cors = require("cors");
 const i18n = require("./lib/i18nConfigure");
-
-require("./lib/connectMongoose");
-
 //const basicAuthMiddleware = require('./lib/basicAuthMiddleware'); // Para control de inicio de sesión
-//const { Session } = require("inspector");
-
 require("./lib/connectMongoose");
+
+const sessionConfig = require("./middlewares/sessionConfig");
+const securityHeaders = require("./middlewares/securityHeaders");
+const { catch404, handleErrors } = require("./middlewares/errorHandlers");
 
 const app = express();
 
-// view engine setup
+//const { Session } = require("inspector");
+
+// VIEW ENGINE SETUP
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 
 app.locals.title = "NodeApp";
 
-// Middleware
-app.use(cors());
-
-app.use(express.static(path.join(__dirname, "../public")));
-console.log("PUBLIC", path.join(__dirname, "/public"));
-
-//app.use((req, res, next) => {
-//  res.setHeader('X-Content-Type-Options', 'nosniff');
-//  next();
-//});
-
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(i18n.init);
-
-//app.use(session({
-//  name: 'nodeapp-session', // nombre de la cookie
-//  secret: 'as98987asd98ashiujkasas768tasdgyy',
-//  saveUninitialized: true, // Forces a session that is "uninitialized" to be saved to the store
-//  resave: false, // Forces the session to be saved back to the session store, even if the session was never modified during the request
-//  cookie: {
-//    maxAge: 1000 * 60 * 60 * 24 * 2 // 2d - expiración de la sesión por inactividad
-//  },
-//  store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI})
-//}));
+// MIDDLEWARE
+app.use(cors()); // Habilita Cors
+app.use(express.static(path.join(__dirname, "../public"))); // Sirve los ficheros estáticos  en la carpeta públic
+app.use(securityHeaders); // Configura encabezados de seguridad
+app.use(logger("dev")); // Regista solicitudes y respuestas en la consola en modo desarrollo.
+app.use(express.json()); // Analizar solicitudes con formato JSON.
+app.use(express.urlencoded({ extended: true })); // Analizar cuerpos de solicitud con tipo de contenido application/x-www-form-urlencoded
+app.use(cookieParser()); // Analizar cookies en las solicitudes
+app.use(i18n.init); // Internacionalización (i18n).
+app.use(sessionConfig); // Configuración de sesiones.
 
 // Hacemos que el objeto session esté disponible al renderizar las vistas
-//app.use((req,res,next) => {
-//  res.locals.session = req.session;
-//  next();
-//});
+app.use((req, res, next) => {
+  res.locals.session = req.session;
+  next();
+});
 
-/**
- * Rutas del website
- */
+// ROUTES
 app.use("/", require("./routes/web"));
 app.use("/api", require("./routes/api"));
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+app.use(catch404);
 
 // error handler
-app.use(function (err, req, res, next) {
-  // comprobar si es un error de validación
-  if (err.array) {
-    const errorInfo = err.array[0];
-    console.log(errorInfo);
-    err.message = `Error en ${errorInfo.location}, parámetro ${errorInfo.path} ${errorInfo.msg}`;
-    err.status = 422;
-  }
-
-  // Si lo que ha fallado es una perición al api, responder con un JSON
-  if (req.originalUrl.startsWith("/api/")) {
-    res.json({ error: err.message });
-    return; // Para que no se siga ejecutando el código de abajo
-  }
-
-  // Set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
+app.use(handleErrors);
 
 module.exports = app;
