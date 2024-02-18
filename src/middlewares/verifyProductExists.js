b6fb6ed237ded3b2d0f90e8f3ac2fd9fc1bd3882
Doplax <doplax@gmail.com>
2024-02-18 10:37:38 +0100

    feat(middleware): add verifyProductExists to check product availability

---
+++ src/middlewares/verifyProductExists.js
@@ -0,0 +1,17 @@
+const Product = require("../models/Product.js");
+const handleHttpError = require("../utils/errorHandler");
+
+const verifyProductExists = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const product = await Product.findById(id);
+    if (!product) {
+      return handleHttpError(res, "PRODUCT_NOT_FOUND", 404);
+    }
+    next();
+  } catch (error) {
+    return handleHttpError(res, "ERROR VERYFING PRODUCT", 500);
+  }
+};
+
+module.exports = verifyProductExists;
