ec126d72c91285819bb77cabdb4a692a2775625b
Doplax <doplax@gmail.com>
2024-09-05 20:38:34 +0200

    feat(api): add Swagger documentation for auth and products routes
    - Added auto-generated Swagger documentation for auth routes
    - Detailed descriptions added for product routes
    - Configured Swagger integration in the main application file

--- src/routes/api/auth.js
+++ src/routes/api/auth.js
@@ -10,12 +10,66 @@ const {
   validatorLogin,
 } = require("../../validators/authValidator.js");
 
-//const { validatorRegister, validatorLogin } = require("../validators/auth");
-
+/**
+ * @swagger
+ * /api/auth/register:
+ *   post:
+ *     summary: Registro de usuarios
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Usuario registrado exitosamente.
+ *       400:
+ *         description: Error de validación.
+ */
 router.post("/register", validatorRegister, registerCtrl);
 
+/**
+ * @swagger
+ * /api/auth/login:
+ *   post:
+ *     summary: Inicio de sesión de usuarios
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Inicio de sesión exitoso.
+ *       401:
+ *         description: Credenciales inválidas.
+ */
 router.post("/login", validatorLogin, loginCtrl);
 
+/**
+ * @swagger
+ * /api/auth/users:
+ *   get:
+ *     summary: Obtiene la lista de todos los usuarios
+ *     tags: [Auth]
+ *     responses:
+ *       200:
+ *         description: Retorna una lista de usuarios.
+ */
 router.get("/users", getAllUsersCtrl);
 
 module.exports = router;
