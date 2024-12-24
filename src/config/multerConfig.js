f17db97427d7d4c41761def2f39a5cd444c7a40c
Doplax <doplax@gmail.com>
2024-12-24 20:06:56 +0100

    refactor(upload): switch file storage from local disk to memory for MongoDB integration

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -1,18 +1,26 @@
 const multer = require("multer");
 const path = require("path");
 
-const filePath = path.resolve(__dirname, "..", "..", "public", "images");
+// SAVE IN LOCAL STORAGE
+//const filePath = path.resolve(__dirname, "..", "..", "public", "images");
 
-const fileStorage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, filePath);
-  },
-  filename: (req, file, callback) => {
-    const fileName =
-      file.fieldname + "-" + Date.now() + "-" + file.originalname;
-    callback(null, fileName);
-  },
-});
+//const fileStorage = multer.diskStorage({
+//  destination: (req, file, callback) => {
+//    callback(null, filePath);
+//  },
+//  filename: (req, file, callback) => {
+//    const fileName =
+//      file.fieldname + "-" + Date.now() + "-" + file.originalname;
+//    callback(null, fileName);
+//  },
+//});
+
+//const upload = multer({
+//  storage: fileStorage,
+//});
+
+// SAVE IN MONGO
+const fileStorage = multer.memoryStorage();
 
 const upload = multer({
   storage: fileStorage,
